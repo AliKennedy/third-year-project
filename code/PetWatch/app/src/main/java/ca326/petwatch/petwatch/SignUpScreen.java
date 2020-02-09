1a76090b8ad1c15920bf1a11be7a688fcef4c874
Ethan Sahrkey <ethan.sharkey3@mail.dcu.ie>
2020-02-09 21:01:39 +0000

    Added firebase functionality to the app

--- code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/SignUpScreen.java
+++ code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/SignUpScreen.java
@@ -26,92 +26,110 @@ public class SignUpScreen extends AppCompatActivity
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
 
-        firstName = (EditText)findViewById(R.id.firstName);
-        lastName = (EditText)findViewById(R.id.Surname);
-        petsName = (EditText)findViewById(R.id.petsName);
-        emailAddress = (EditText)findViewById(R.id.emailAddress);
-        createPassword = (EditText)findViewById(R.id.passwordCreate);
-        confirmPassword = (EditText)findViewById(R.id.confirmPassword);
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.Surname);
+        petsName = (EditText) findViewById(R.id.petsName);
+        emailAddress = (EditText) findViewById(R.id.emailAddress);
+        createPassword = (EditText) findViewById(R.id.passwordCreate);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
 
-        buttonSignUp = (Button)findViewById(R.id.buttonSignUp);
-        passwordNotMatch = (TextView)findViewById(R.id.passwordNotMatch);
+        buttonSignUp = (Button) findViewById(R.id.buttonSignUp);
+        passwordNotMatch = (TextView) findViewById(R.id.passwordNotMatch);
 
         mDatabaseHelper = new DatabaseHelper(this);
-
-        buttonSignUp.setOnClickListener(new View.OnClickListener()
-        {
-
-                @Override
-                public void onClick(View view)
-                {
-                    if (passwordMatch(createPassword.getText().toString(), confirmPassword.getText().toString())) //&& (emailAlreadyTaken(emailAddress.getText().toString()) != true))
-                    {
-                        if ((firstName.getText().toString().length() != 0) && (lastName.getText().toString().length() != 0) && (petsName.getText().toString().length() != 0) && (emailAddress.getText().toString().length() != 0))
-                            createAccount(firstName.getText().toString(), lastName.getText().toString(), petsName.getText().toString(), emailAddress.getText().toString(), confirmPassword.getText().toString(), 1);
-                        else
-                            toastMessage("You must fill in all parameters!");
-                    }
-                        else
-                    {
-                        passwordMatch(createPassword.getText().toString(), confirmPassword.getText().toString());
-                        //emailAlreadyTaken(emailAddress.getText().toString());
-                    }
-                }
-
-        });
     }
 
-    public void createAccount(String fName, String lName, String pName, String eAddress, String password, Integer ardID)
-    {
-        boolean db2 = mDatabaseHelper.addData(fName, "fName");
-        boolean db3 = mDatabaseHelper.addData(lName, "sName");
-        boolean db4 = mDatabaseHelper.addData(pName, "pName");
-        boolean db5 = mDatabaseHelper.addData(eAddress, "eMail");
-        boolean db6 = mDatabaseHelper.addData(password, "pWord");
-        boolean db7 = mDatabaseHelper.addData(ardID.toString(), "ardID");
-
-        if ((db2) && (db3) && (db4) && (db5) && (db6) && (db7))
-            toastMessage("Hoorraayy it Works");
-        else
-            toastMessage("I broke it");
 
 
-    }
 
-    public boolean passwordMatch(String password, String otherPassword)
-    {
-        if (password.equals(otherPassword))
-            return true;
-        else
-        {
-            TextView warning = SignUpScreen.this.passwordNotMatch;
-            warning.setVisibility(View.VISIBLE);
-            return false;
-        }
-    }
 
-    private void toastMessage(String message)
-    {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
 
-// NEED DATABASE SETUP TO USE THIS METHOD
-//    public boolean emailAlreadyTaken(String email)
-//    {
-//        //Database query here
-//        if (true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        buttonSignUp.setOnClickListener(new View.OnClickListener()
 //        {
-//            //Display error saying already taken
+//
+//                @Override
+//                public void onClick(View view)
+//                {
+//                    if (passwordMatch(createPassword.getText().toString(), confirmPassword.getText().toString())) //&& (emailAlreadyTaken(emailAddress.getText().toString()) != true))
+//                    {
+//                        if ((firstName.getText().toString().length() != 0) && (lastName.getText().toString().length() != 0) && (petsName.getText().toString().length() != 0) && (emailAddress.getText().toString().length() != 0))
+//                            createAccount(firstName.getText().toString(), lastName.getText().toString(), petsName.getText().toString(), emailAddress.getText().toString(), confirmPassword.getText().toString(), 1);
+//                        else
+//                            toastMessage("You must fill in all parameters!");
+//                    }
+//                        else
+//                    {
+//                        passwordMatch(createPassword.getText().toString(), confirmPassword.getText().toString());
+//                        //emailAlreadyTaken(emailAddress.getText().toString());
+//                    }
+//                }
+//
+//        });
+//    }
+//
+//    public void createAccount(String fName, String lName, String pName, String eAddress, String password, Integer ardID)
+//    {
+//        boolean db2 = mDatabaseHelper.addData(fName, "fName");
+//        boolean db3 = mDatabaseHelper.addData(lName, "sName");
+//        boolean db4 = mDatabaseHelper.addData(pName, "pName");
+//        boolean db5 = mDatabaseHelper.addData(eAddress, "eMail");
+//        boolean db6 = mDatabaseHelper.addData(password, "pWord");
+//        boolean db7 = mDatabaseHelper.addData(ardID.toString(), "ardID");
+//
+//        if ((db2) && (db3) && (db4) && (db5) && (db6) && (db7))
+//            toastMessage("Hoorraayy it Works");
+//        else
+//            toastMessage("I broke it");
+//
+//
+//    }
+//
+//    public boolean passwordMatch(String password, String otherPassword)
+//    {
+//        if (password.equals(otherPassword))
 //            return true;
-//        }
 //        else
 //        {
+//            TextView warning = SignUpScreen.this.passwordNotMatch;
+//            warning.setVisibility(View.VISIBLE);
 //            return false;
 //        }
 //    }
+//
+//    private void toastMessage(String message)
+//    {
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//    }
+//
+//// NEED DATABASE SETUP TO USE THIS METHOD
+////    public boolean emailAlreadyTaken(String email)
+////    {
+////        //Database query here
+////        if (true)
+////        {
+////            //Display error saying already taken
+////            return true;
+////        }
+////        else
+////        {
+////            return false;
+////        }
+////    }
 }
