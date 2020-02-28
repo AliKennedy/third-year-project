26d30866b7c136e7bdc46462f8a1a3969a0ab80d
Ali <alison.kennedy55@mail.dcu.ie>
2020-02-28 20:11:21 +0000

    Changed nav bar to have white icons

--- code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/Main2Activity.java
+++ code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/Main2Activity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -14,6 +15,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
+import java.util.HashMap;
 
 import static ca326.petwatch.petwatch.R.id.navigation_speed;
 
@@ -25,6 +27,7 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null);
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -34,7 +37,6 @@ public class Main2Activity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
 
 
 
