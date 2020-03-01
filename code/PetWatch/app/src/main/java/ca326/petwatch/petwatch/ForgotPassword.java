68a453cef9b6f28139713dae1e832202b179bd51
Ethan Sahrkey <ethan.sharkey3@mail.dcu.ie>
2020-03-01 17:19:06 +0000

    Added more capabilities to the Forgot Password class

--- code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/ForgotPassword.java
+++ code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/ForgotPassword.java
@@ -2,10 +2,12 @@ package ca326.petwatch.petwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -33,8 +35,16 @@ public class ForgotPassword extends AppCompatActivity
             public void onClick(View v)
             {
                 firebaseAuth.sendPasswordResetEmail(enterEmail.getText().toString().trim());
+                toastMessage("Email Sent!");
+                Intent intent = new Intent(ForgotPassword.this, SignInScreen.class);
+                startActivity(intent);
             }
         });
 
+    }
+
+    public void toastMessage(String s)
+    {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
