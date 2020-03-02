6b3cd82cfaf05ffb0189aa2d3908020cbdcaabe2
Ali <alison.kennedy55@mail.dcu.ie>
2020-03-02 00:12:33 +0000

    Support page with contact form.

--- code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/ui/settings/AboutInfo.java
+++ code/PetWatch/app/src/main/java/ca326/petwatch/petwatch/ui/settings/AboutInfo.java
@@ -2,12 +2,16 @@ package ca326.petwatch.petwatch.ui.settings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ca326.petwatch.petwatch.R;
 
 public class AboutInfo extends AppCompatActivity
 {
+    private Button supportButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,7 +19,17 @@ public class AboutInfo extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_info);
 
-        //Info about us bbz
+        supportButton = findViewById(R.id.AboutContactButton);
+
+        supportButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(AboutInfo.this, SupportInfo.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
